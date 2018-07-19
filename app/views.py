1d587f7b146befbc7a6c417f9ad925db1c2a36c4
Anas <anas.ansal10@gmail.com>
2018-07-20 03:03:39 +0530

    adding bug

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 import json
 from _ast import Dict
-from datetime import datetime, timedelta
 
 import pytz
 from django.http import JsonResponse
 
 from libs import youtube_dl
+from datetime import datetime, time, timedelta
+from django.shortcuts import render
 
 expired_days = 30
 
@@ -13,34 +14,29 @@ expired_days = 30
 from app.models import Video
 
 
-def load_video_config(video_url: str) :
+def load_video_config(video_url: str) -> Video:
     video: Video = Video.objects.filter(video_url=video_url).first()
     if video and video.updated_at + timedelta(days=expired_days) > datetime.now(pytz.UTC):
-        return False, video
+        return video
     else:
         config = youtube_dl._real_main([video_url, "-F", "--no-check-certificate"])[0]
         config = json.dumps(config)
         video: Video = Video.objects.create(video_url=video_url, video_config=config)
-        return True, video
+
+    return video
 
 
 def video_config(request):
     data: Dict[str, str] = json.loads(request.body)
     video_url: str = data['video_url']
-    (modified, video) = load_video_config(video_url)
-    if modified:
-        return JsonResponse({'data': json.loads(video.video_config)}, status=200)
-    else:
-        return JsonResponse({'data': json.loads(video.video_config)}, status=304)
+    video: Video = load_video_config(video_url)
+    return JsonResponse({'data': json.loads(video.video_config)})
 
 
 def video_format(request):
     data: Dict[str, str] = json.loads(request.body)
     video_url: str = data['video_url']
-    modified, video = load_video_config(video_url)
+    video: Video = load_video_config(video_url)
     config: Dict[str, str] = json.loads(video.video_config)
     formats = config.get('format') or config.get('formats')
-    if modified:
-        return JsonResponse({'data': formats})
-    else:
-        return JsonResponse({'data': formats}, status=304)
+    return JsonResponse({'data': formats})
