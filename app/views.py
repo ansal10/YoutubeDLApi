5b29218b469f693cc385537a63d54edb1673313b
Anas <anas.ansal10@gmail.com>
2018-07-20 02:51:53 +0530

    adding 304 status

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 import json
-from _ast import Dict
+from _ast import Dict, Tuple
 
 import pytz
 from django.http import JsonResponse
+from pip._vendor.urllib3.contrib._securetransport.bindings import Boolean
 
 from libs import youtube_dl
 from datetime import datetime, time, timedelta
@@ -14,29 +15,34 @@ expired_days = 30
 from app.models import Video
 
 
-def load_video_config(video_url: str) -> Video:
+def load_video_config(video_url: str) -> (Boolean, Video):
     video: Video = Video.objects.filter(video_url=video_url).first()
     if video and video.updated_at + timedelta(days=expired_days) > datetime.now(pytz.UTC):
-        return video
+        return False, video
     else:
         config = youtube_dl._real_main([video_url, "-F", "--no-check-certificate"])[0]
         config = json.dumps(config)
         video: Video = Video.objects.create(video_url=video_url, video_config=config)
-
-    return video
+        return True, video
 
 
 def video_config(request):
     data: Dict[str, str] = json.loads(request.body)
     video_url: str = data['video_url']
-    video: Video = load_video_config(video_url)
-    return JsonResponse({'data': json.loads(video.video_config)})
+    (modified, video) = load_video_config(video_url)
+    if modified:
+        return JsonResponse({'data': json.loads(video.video_config)}, status=200)
+    else:
+        return JsonResponse({'data': json.loads(video.video_config)}, status=304)
 
 
 def video_format(request):
     data: Dict[str, str] = json.loads(request.body)
     video_url: str = data['video_url']
-    video: Video = load_video_config(video_url)
+    modified, video = load_video_config(video_url)
     config: Dict[str, str] = json.loads(video.video_config)
     formats = config.get('format') or config.get('formats')
-    return JsonResponse({'data': formats})
+    if modified:
+        return JsonResponse({'data': formats})
+    else:
+        return JsonResponse({'data': formats}, status=304)
